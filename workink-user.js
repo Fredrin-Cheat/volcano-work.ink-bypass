87e5d94004381fdebc92dddff46988a182b9eb8e
Fredrin-Cheat <khesyaaurell7@gmail.com>
2025-11-23 15:07:01 +0700

    workink-user.js

---
+++ workink-user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Volcano & Work.ink Bypass
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0.0
+// @description  Bypass Volcano and Work.ink links with unified panel
+// @author       Vynzz
+// @match        https://key.volcano.wtf/*
+// @match        https://work.ink/*
+// @run-at       document-start
+// @grant        unsafeWindow
+// @downloadURL  https://github.com/Difz25x/volcano-bypass/raw/refs/heads/main/workink.user.js
+// @updateURL    https://github.com/Difz25x/volcano-bypass/raw/refs/heads/main/workink.user.js
+// @require      https://github.com/Difz25x/volcano-bypass/raw/refs/heads/main/workink-bypass.js
+// ==/UserScript==
