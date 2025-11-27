777b926ad2427ecf64ca97c01e926194d60689c7
Fredrin-Cheat <khesyaaurell7@gmail.com>
2025-11-27 19:27:43 +0700

    luarmoruser.js

---
+++ luarmoruser.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Luarmor Bypass?
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0.0
+// @description  Bypass AdsLuarmor
+// @author       Nadhif
+// @match        https://ads.luarmor.net/*
+// @match        https://work.ink/*
+// @run-at       document-start
+// @grant        unsafeWindow
+// @downloadURL  https://github.com/elfuhh/bypass/raw/main/bypass.user.js
+// @updateURL    https://github.com/elfuhh/bypass/raw/main/bypass.user.js
+// @require https://github.com/elfuhh/bypass/raw/refs/heads/main/bypass.js
+// ==/UserScript==
