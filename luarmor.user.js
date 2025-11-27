d93bc933e946a302fb8ede3ef8881d28f1d4429f
Fredrin-Cheat <khesyaaurell7@gmail.com>
2025-11-27 19:32:26 +0700

    luarmor.user.js

--- luarmor.user.js
+++ luarmor.user.js
@@ -8,7 +8,7 @@
 // @match        https://work.ink/*
 // @run-at       document-start
 // @grant        unsafeWindow
-// @downloadURL  https://github.com/Fredrin-Cheat/volcano-work.ink-bypass/raw/refs/heads/main/luarmoruser.js
-// @updateURL    https://github.com/Fredrin-Cheat/volcano-work.ink-bypass/raw/refs/heads/main/luarmoruser.js
+// @downloadURL  https://github.com/Fredrin-Cheat/volcano-work.ink-bypass/raw/refs/heads/main/luarmor.user.js
+// @updateURL    https://github.com/Fredrin-Cheat/volcano-work.ink-bypass/raw/refs/heads/main/luarmor.user.js
 // @require      https://raw.githubusercontent.com/Fredrin-Cheat/volcano-work.ink-bypass/refs/heads/main/luarmorbypass.js
 // ==/UserScript==
